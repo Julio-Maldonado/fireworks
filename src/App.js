2c5f2196270ad9fabebc617b5281104aeefed5a5
Julio Maldonado <juliom@Julios-MacBook-Pro-3.local>
2019-06-11 12:38:43 -0500

    updated time

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends React.Component {
   prevPos = {offsetX: 0, offsetY: 0}
 
   componentDidMount() {
-    setInterval(this.update, 50)
+    setInterval(this.update, 75)
     // document.body.addEventListener('touchstart', this.preventMotion, false)
     // document.body.addEventListener('touchmove', function() {})
     // window.addEventListener('touchmove', function() {})
